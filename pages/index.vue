bada79e4a883822d91fee1f20fbfaf1781b4b34f
liangwei <banemail@gmail.com>
2020-07-10 12:17:46 +0800

    23-获取真实轮播图数据, 显示首页背景

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,65 @@
 <template>
-  <el-carousel indicator-position="none">
+    <div class="container">
+        <!-- 幻灯片 -->
+        <el-carousel :interval="5000" arrow="always">
+
+            <el-carousel-item v-for="(item, index) in banners" :key="index">
+                
+                
+                <div
+                    class="banner-image"
+                    :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+                ></div>
 
 
-    <el-carousel-item >
-      <h3>1</h3>
-    </el-carousel-item>
-    <el-carousel-item >
-      <h3>2</h3>
-    </el-carousel-item>
-    <el-carousel-item >
-      <h3>3</h3>
-    </el-carousel-item>
-    <el-carousel-item >
-      <h3>4</h3>
-    </el-carousel-item>
+            </el-carousel-item>
 
 
-
-  </el-carousel>
+        </el-carousel>
+    </div>
 </template>
 
 <script>
 export default {
-  
-}
+  data() {
+    return {
+      banners: []
+    }
+  },
+  created() {
+    // 创建页面时, 需要获取数据
+    this.$axios({
+      url: '/scenics/banners'
+    }).then(res=>{
+      console.log(res.data);
+      this.banners = res.data.data.map(img=>{
+        return {
+          ...img,
+          url: this.$axios.defaults.baseURL + img.url
+        }
+      })
+      
+    })
+  }
+};
 </script>
 
-<style>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
-  }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
+
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
+}
 </style>
