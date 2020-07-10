1426be30a323234e57cef9967819a6085b2cfc30
liangwei <banemail@gmail.com>
2020-07-10 11:54:47 +0800

    轮播图默认写法

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>首页</div>
+  <el-carousel indicator-position="none">
+
+
+    <el-carousel-item >
+      <h3>1</h3>
+    </el-carousel-item>
+    <el-carousel-item >
+      <h3>2</h3>
+    </el-carousel-item>
+    <el-carousel-item >
+      <h3>3</h3>
+    </el-carousel-item>
+    <el-carousel-item >
+      <h3>4</h3>
+    </el-carousel-item>
+
+
+
+  </el-carousel>
 </template>
 
 <script>
 export default {
-
+  
 }
 </script>
 
 <style>
-
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 18px;
+    opacity: 0.75;
+    line-height: 300px;
+    margin: 0;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
