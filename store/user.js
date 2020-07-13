376a1fa751513df656f8ec50bd21c77a430c9454
liangwei <banemail@gmail.com>
2020-07-13 09:46:15 +0800

    05-vuex 创建登录 action

--- store/user.js
+++ store/user.js
@@ -13,4 +13,24 @@ export const mutations = {
     }
 };
 
-export const actions = {};
+export const actions = {
+    login(store, data) {
+        // 这个函数可以接收到两个参数
+        // 1. 当前仓库 store 的本身
+        // 2. 外部调用时额外的数据
+        // 对于登录来说就是表单数据对象
+        this.$axios({
+            url: "/accounts/login",
+            method: "POST",
+            data
+        }).then(res => {
+            console.log(res.data);
+            if (res.data.token) {
+                // action 是 vuex 执行异步操作的地方
+                // 但是修改 state 数据依然只能通过 mutations
+                // this.$store.commit('user/setUserInfo', res.data)
+                store.commit('setUserInfo', res.data)
+            }
+        })
+    }
+};
