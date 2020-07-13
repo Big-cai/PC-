16ae8c0bfc1576c754f84cc3b92da54be20acbc3
liangwei <banemail@gmail.com>
2020-07-13 11:43:09 +0800

    17-表单数据的校验

--- components/user/RegisterForm.vue
+++ components/user/RegisterForm.vue
@@ -4,13 +4,13 @@
         ref="form" 
         :rules="rules" 
         class="form">
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="username">
                 <el-input v-model="form.username"
                 placeholder="用户名手机">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="captcha">
                 <el-input v-model="form.captcha"
                 placeholder="验证码" >
                     <template slot="append">
@@ -21,20 +21,20 @@
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="nickname">
                 <el-input v-model="form.nickname"
                 placeholder="你的名字">
                 </el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="password">
                 <el-input v-model="form.password"
                 placeholder="密码" 
                 type="password"
                 ></el-input>
             </el-form-item>
 
-            <el-form-item class="form-item">
+            <el-form-item class="form-item" prop="checkPassword">
                 <el-input v-model="form.checkPassword"
                 placeholder="确认密码" 
                 type="password">
@@ -63,7 +63,43 @@ export default {
                 captcha: ""		// 手机验证码
             },
             // 表单规则
-            rules: {},
+            rules: {
+                username: [
+                    {
+                        required: true,
+                        message: '请输入手机号',
+                        trigger: 'blur'
+                    }
+                ],
+                password: [
+                    {
+                        required: true,
+                        message: '请输入密码',
+                        trigger: 'blur'
+                    }
+                ],
+                checkPassword: [
+                    {
+                        required: true,
+                        message: '请再次输入密码',
+                        trigger: 'blur'
+                    }
+                ],
+                nickname: [
+                    {
+                        required: true,
+                        message: '请输入昵称',
+                        trigger: 'blur'
+                    }
+                ],
+                captcha: [
+                    {
+                        required: true,
+                        message: '请输入手机验证码',
+                        trigger: 'blur'
+                    }
+                ],
+            },
         }
     },
     methods: {
