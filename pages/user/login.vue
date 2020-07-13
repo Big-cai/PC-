092adfd01c073c86799cf172991316a034a8dd06
liangwei <banemail@gmail.com>
2020-07-13 11:21:46 +0800

    15-注册表单基本布局

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -22,7 +22,7 @@
                 <LoginForm v-if="currentTab == 0"/>
 
                 <!-- 注册功能组件 -->
-                <!-- <RegisterForm v-if="currentTab == 1"/> -->
+                <RegisterForm v-if="currentTab == 1"/>
             </div>
         </el-row>
     </div>
@@ -30,9 +30,12 @@
 
 <script>
 import LoginForm from '@/components/user/LoginForm'
+import RegisterForm from '@/components/user/RegisterForm'
+
 export default {
     components: {
-      LoginForm
+      LoginForm,
+      RegisterForm
     },
     data(){
         return {
