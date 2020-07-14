880271288faa074c57b648d3468948f7a8fb5d64
liangwei <banemail@gmail.com>
2020-07-14 12:20:38 +0800

    优化体验设置

--- components/air/SearchForm.vue
+++ components/air/SearchForm.vue
@@ -17,6 +17,8 @@
                 <el-autocomplete
                 v-model="form.departCity"
                 :fetch-suggestions="queryDepartSearch"
+                :trigger-on-focus="false"
+                :highlight-first-item ="true"
                 placeholder="请搜索出发城市"
                 @select="handleDepartSelect"
                 class="el-autocomplete"
@@ -26,6 +28,8 @@
                 <el-autocomplete
                 v-model="form.destCity"
                 :fetch-suggestions="queryDestSearch"
+                :trigger-on-focus="false"
+                :highlight-first-item ="true"
                 placeholder="请搜索到达城市"
                 @select="handleDestSelect"
                 class="el-autocomplete"
