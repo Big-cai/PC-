ba2970e3f691013648dd4d66258527597278708a
liangwei <banemail@gmail.com>
2020-07-14 10:04:14 +0800

    08-日期选择组件的用法

--- components/air/SearchForm.vue
+++ components/air/SearchForm.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-      <el-autocomplete
-        v-model="value"
-        :fetch-suggestions="loadList"
-      ></el-autocomplete>
+      <el-date-picker
+        v-model="valueDate"
+        type="date"
+        placeholder="选择日期">
+    </el-date-picker>
+    {{valueDate}}
   </div>
 </template>
 
@@ -11,30 +13,7 @@
 export default {
     data() {
         return {
-            value: ''
-        }
-    },
-    methods: {
-        loadList(inputValue, showList) {
-            // 这个函数是自动补全的回调函数
-            // 用来提供补全的建议数据数组
-            // 有两个参数
-            // 1. val 是当前输入值
-            // 2. 回调函数, 用来将准备好的数组返回出去
-
-            // 准备的数据格式是固定的, 是一个数组
-            // 数组里面每一个对象都是一条建议
-            const dataSuggestion = [
-                {
-                    // 建议对象里面最重要的一个属性是 value
-                    // 这个值最终会被显示在列表中
-                    value: '小笼包'
-                },
-                {
-                    value: '虾饺'
-                }
-            ]
-            showList(dataSuggestion)
+            valueDate: ''
         }
     }
 }
