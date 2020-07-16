4be5b9e89da0df2631c1a3ad97e677d226414db8
liangwei <banemail@gmail.com>
2020-07-16 11:39:51 +0800

    机票列表页基本布局

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>飞机票列表</div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    过滤条件
+                    
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <div>
+                    航班头部布局
+                    
+                </div>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                    航班信息
+                    
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                侧边栏
+                <!-- 侧边栏组件 -->
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
@@ -16,6 +47,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
 
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
