992af7cf9f83078719cbea908a78ca306d7bdb22
liangwei <banemail@gmail.com>
2020-07-17 09:06:14 +0800

    扩展, 每次默认值不同的小球

---
+++ components/testball.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ball" :style="`background: ${options.bg};`">
+      {{options.num}}
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {
+                    bg: 'blue',
+                    num: (Math.random()*35+1).toFixed(0)
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.ball {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    line-height: 60px;
+    text-align: center;
+    color: white;
+}
+</style>
