c8992de16b7bd727d9c900b4c0df4e08783e0b79
liangwei <banemail@gmail.com>
2020-07-18 10:05:38 +0800

    08-死数据渲染 vuex 历史搜索记录

--- store/history.js
+++ store/history.js
@@ -1,3 +1,25 @@
 export const state = () => ({
-    list: '这里是列表'
+    list: [
+        {
+            departCity: '上海',
+            departCode: 'SHA',
+            destCity: '北京',
+            destCode: 'BJS',
+            departDate: '2020-07-29'
+        },
+        {
+            departCity: '南京',
+            departCode: 'SHA',
+            destCity: '深圳',
+            destCode: 'BJS',
+            departDate: '2020-07-29'
+        },
+        {
+            departCity: '广州',
+            departCode: 'SHA',
+            destCity: '西安',
+            destCode: 'BJS',
+            departDate: '2020-07-29'
+        }
+    ]
 })
