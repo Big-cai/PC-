0b9c99edb9b184b0189eba9924d6f445c3a4d186
liangwei <banemail@gmail.com>
2020-07-18 11:42:32 +0800

    16-创建订单表单组件

--- pages/air/order.vue
+++ pages/air/order.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-      这里是订单页
-  </div>
+    <div class="container">
+        <el-row type="flex" justify="space-between">
+            <!-- 订单表单 -->
+            <OrderForm/>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                侧边栏      
+            </div>
+        </el-row>
+    </div>
 </template>
 
 <script>
+import OrderForm from '@/components/air/OrderForm'
 export default {
-
+    components: {
+        OrderForm
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .container{
+        width:1000px;
+        margin:20px auto;
+    }
+    
+    /*aside*/
+    .aside{
+        width: 350px;
+        height: fit-content;
+        border:1px #ddd solid;
+    }
 </style>
