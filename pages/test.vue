555a72233402129ed53ca664fd5e5c9b2a2c4894
liangwei <banemail@gmail.com>
2020-07-20 09:05:10 +0800

    04-计算属性的惰性求值

---
+++ pages/test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{showMsg()}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+      {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        msg() {
+            console.log('计算属性被执行');
+            return 'hello vue'
+        }
+    },
+    methods: {
+        showMsg() {
+            console.log('函数被执行');
+            return 'hello vue'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
