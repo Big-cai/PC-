b416e3e86895242dcbce7cc3b56bd9029ae65853
liangwei <banemail@gmail.com>
2020-07-24 10:55:59 +0800

    测试动态路由匹配

---
+++ pages/hotel/_id.html.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>这里是我们的动态路由匹配路径</h1>
+      <div>我们的动态路由 id =  {{$route.params.id}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
