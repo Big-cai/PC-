9a9e30d5c8d4555c7b3962966165f7c248b06ff7
liangwei <banemail@gmail.com>
2020-07-24 09:50:47 +0800

    添加点标记

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -11,7 +11,22 @@ export default {
     mounted() {
         // 3. 编写回调函数创建地图
         window.onLoad  = function(){
-          var map = new AMap.Map('mymap');
+          var map = new AMap.Map('mymap', {
+              zoom: 21,
+              center: [113.32459, 23.10668],
+              viewMode: '3D'
+          });
+
+          var marker = new AMap.Marker({
+            position: [113.32459, 23.10668],
+            offset: new AMap.Pixel(-100, -100),
+            icon: '//vdata.amap.com/icons/b18/1/2.png',
+            title: '这里是小蛮腰',
+            content: '<h1>哈哈哈</h1>'
+          })
+
+          map.add(marker)
+          
         }
 
         // 2. 引入高德地图库
