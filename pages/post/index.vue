4905db44f1293b05a8569ab93697679d4461319b
liangwei <banemail@gmail.com>
2020-07-24 10:55:49 +0800

    调用富文本框

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <h1>添加攻略</h1>
-    <VueEditor v-model="content"/>
+    <!-- 由于这个组件只有客户端才会引入
+    所以也只能在客户端使用
+    可以利用 nuxt 提供的标签 client-only
+    声明一部分代码只在客户端处理 -->
+    <client-only>
+      <VueEditor v-model="content"/>
+    </client-only>
   </div>
 </template>
 
