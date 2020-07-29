0997037aea3fde639e04dd9c53d2c073b79aad8d
wweilie <l>
2020-07-30 01:09:30 +0800

    评价模块环形进度条的完善

--- components/hotel/details/ProgressView.vue
+++ components/hotel/details/ProgressView.vue
@@ -1,15 +1,44 @@
 <template>
-<!-- 环形进度条 -->
+  <!-- 环形进度条 -->
   <div>
-    <el-progress type="circle" :percentage="70">
+    <div class="text-hd">
+      <el-progress
+        type="circle"
+        :percentage="77"
+        color="#ff9900"
+        :show-text="false"
+        :stroke-width="2"
+        :width="70"
+      >
+      </el-progress>
+      <div class="text">
         <span>环境</span>
-        <div>产品</div>
-    </el-progress>
+        <span>8.3</span>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {}
+};
 </script>
 
-<style></style>
+<style lang="less">
+.text-hd {
+  position: relative;
+  width: 70px;
+
+  .text {
+    position: absolute;
+    background-color: #fff;
+    text-align: center;
+    color: rgb(255, 153, 0);
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+  }
+}
+</style>
